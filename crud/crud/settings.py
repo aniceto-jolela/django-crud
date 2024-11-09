dd6e1ff02146349acc860023771c767cfcbc535e
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-11-09 21:23:09 +0100

    problem field_is  resolved

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = "static/"
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
 
-CRISPY_TEMPLATE_PACK = 'foundation'
+CRISPY_TEMPLATE_PACK = 'foundation-6'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
