78ddc71ece72f8ff307b6234481265ae5025dfb1
aniceto-jolela <anicetojosejolelaj@gmail.com>
2024-11-19 18:23:15 +0100

    update.

--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'home'
@@ -129,7 +129,8 @@ CRISPY_TEMPLATE_PACK = 'foundation-6'
 
 # Media files (user uploads)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory for uploaded media files
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # Directory for uploaded media files
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Static files (CSS, JavaScript, Images)
 # To deploy
